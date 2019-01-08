8422151c944003c54f9a3642429c5cb16d85d405
Matteo Salvi <salvadorbs@gmail.com>
2019-01-08 11:30:37 +0100

    Improved OutputView page

--- client/src/views/Containers/CurrentWeather.js
+++ client/src/views/Containers/CurrentWeather.js
@@ -4,11 +4,13 @@ import { Link } from 'react-router-dom';
 import { weatherAppAPI } from '../../utils';
 
 // Assets
-import ThunderStormIcon from '../assets/weather_icons/01W.svg';
-import RainIcon from '../assets/weather_icons/02W.svg';
-import SnowIcon from '../assets/weather_icons/03W.svg';
-import ClearIcon from '../assets/weather_icons/04W-DAY.svg';
-import CloudsIcon from '../assets/weather_icons/05W.svg';
+import ThunderStormIcon from '../assets/weather_icons/thunder.svg';
+import RainIcon from '../assets/weather_icons/rainy-5.svg';
+import SnowIcon from '../assets/weather_icons/snowy.svg';
+import ClearIcon from '../assets/weather_icons/day.svg';
+import CloudsIcon from '../assets/weather_icons/cloudy.svg';
+import FoggyIcon from '../assets/weather_icons/foggy.svg';
+
 import NoLocationFound from '../assets/no-location.svg';
 import LoadingIcon from '../assets/loading.svg';
 
@@ -24,7 +26,7 @@ class CurrentWeather extends Component {
       windDirection: '',
       currentCondition: '',
       currentConditionDescription: '',
-      weatherIcon: '',
+      weatherIcon: ClearIcon,
       cityName: '',
       cityNotFound: '',
     });
@@ -50,6 +52,8 @@ class CurrentWeather extends Component {
       this.setState({ weatherIcon: RainIcon });
     } else if (weatherId >= 600 && weatherId <= 622) {
       this.setState({ weatherIcon: SnowIcon });
+    } else if (weatherId === 741 || weatherId === 701) {
+      this.setState({ weatherIcon: FoggyIcon});
     } else if (weatherId === 800) {
       this.setState({ weatherIcon: ClearIcon });
     } else if (weatherId >= 801 && weatherId <= 804) {
@@ -58,9 +62,9 @@ class CurrentWeather extends Component {
 
     this.setState({
       isLoading: false,
-      currentTemp: Math.round(ApiResponseData.data.main.temp) + '°',
+      currentTemp: Math.round(ApiResponseData.data.main.temp - 273.15) + '°',
       humidity: ApiResponseData.data.main.humidity + '%',
-      wind: Math.round(ApiResponseData.data.wind.speed) + ' mph',
+      wind: Math.round(ApiResponseData.data.wind.speed),
       windDirection: ApiResponseData.data.wind.deg,
       currentCondition: ApiResponseData.data.weather[0].main,
       currentConditionDescription: ApiResponseData.data.weather[0].description,
@@ -96,26 +100,30 @@ class CurrentWeather extends Component {
 
     const WeatherConditions = (
       this.state.cityNotFound === 404 ? <div> {WeatherCardError} </div> :
+        <div id="outputView">
+          <p id="currently" className="card__title">{this.state.currentCondition}</p>
+          <p id="location" className="card__subtitle">in {this.state.cityName}</p>
 
-        <div>
-          <div className='homeBtn'>
-            <Link to='/'><button>Home</button></Link>
-          </div>
-          <div className='weatherCardContainer'>
-            <div className='weatherCard'>
-              <img src={this.state.weatherIcon} alt='Weather icon' />
-              <div className='conditionsOverview'>
-                <p>{this.state.currentTemp}</p>
-                <p>{this.state.currentConditionDescription}</p>
-              </div>
-              <div className='conditionDetails'>
-                <p>Humidity: {this.state.humidity} </p>
-                <p>Wind Speed: {this.state.wind} </p>
-              </div>
-            </div>
+          <img src={this.state.weatherIcon} alt='Weather icon' />
 
-            <h4> Location | {this.state.cityName} </h4>
-          </div>
+          <ul id="weather" className="weather-details">
+            <li className="weather-details__item">
+              <span className="weather-details__label">Humidity</span>
+              <span className="weather-details__value"><span id="humidity">{this.state.humidity}</span></span>
+            </li>
+            <li className="weather-details__item">
+              <span className="weather-details__label">Temperature</span>
+              <span className="weather-details__value"><span id="temp">{this.state.currentTemp}</span></span>
+            </li>
+            <li className="weather-details__item">
+              <span className="weather-details__label">Wind</span>
+              <span className="weather-details__value"><span id="wind">{this.state.wind}</span><small>MPH</small></span>
+            </li>
+          </ul>
+
+          <Link to='/'>
+            <button className='button button--small reset'>Check Another Location</button>
+          </Link>
         </div>
     );
 
@@ -124,7 +132,6 @@ class CurrentWeather extends Component {
         <p className='card__title'>Loading...</p>
         <img className='loadingIcon' src={LoadingIcon} alt='loading icon' />
       </div>
-
     );
 
     const CurrentWeatherCard = (
